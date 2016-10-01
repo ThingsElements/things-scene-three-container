0e77b868d0b9325b2a4588447a32fed3ce50c7ea
heartyoh <heartyoh@gmail.com>
2016-10-01 21:50:14 +0900

    0.0.45

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import Component3d from './component-3d'
+export { default as Component3d } from './component-3d'
+
 export { default as ThreeContainer } from './three-container'
 export { default as VideoPlayer360 } from './video-player-360'
+
+export { default as Door } from './door'
+export { default as ForkLift } from './forkLift'
+export { default as HumiditySensor } from './humidity-sensor'
+export { default as Person } from './person'
+export { default as Plane } from './plane'
+export { default as Rack } from './rack'
+export { default as Stock } from './stock'
